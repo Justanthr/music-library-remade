3f076ff7267250b323ce6c8d2d7d48b923030213
Jennika Wilson <j.r.a.112100@gmail.com>
2022-06-18 12:25:18 -0400

    code along 2 done

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,11 +1,19 @@
-import GalleryItem from './GalleryItem.js'
+import React from 'react';
+import GalleryItem from './GalleryItem';
 
-function Gallery(props){
-    return (
-        <div>
-            <GalleryItem />
-        </div>
-    )
+function Gallery(props) {
+
+    const display = props.data.map((item, index) => {
+        return (
+            <GalleryItem item={item} key={index} />
+        )
+    })
+
+  return (
+    <div>
+        {display}
+    </div>
+  )
 }
 
 export default Gallery
